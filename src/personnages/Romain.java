6fc4054c2b53cb580039b6560877022dbb88d0d3
SERGI <SERGI@LAPTOP-U1GA6VM1.lan>
2022-11-11 22:38:25 +0100

    tp2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,12 +1,16 @@
 package personnages;
+import personnages.Equipement;
 
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement equipement [];
+	private int nbEquipement = 0;
 	public Romain(String nom, int force) {
 		assert force>0 : "la force d’un Romain est toujours positive";
 		this.nom = nom;
 		this.force = force;
+		equipement = new Equipement[2];
 	}
 	public String getNom() {
 		return nom;
@@ -28,6 +32,21 @@ public class Romain {
 			parler("J'abandonne...");
 		}
 		
+	public int sEquiper (equipement, nbEquipement) {
+		
+		
+		switch(nbEquipement) {
+			case 1: 
+				System.out.println("le soldat" + getNom()+ " est dejà bien protegé !");
+			case 2:
+				System.out.println("Le soldat" + getNom() + " possède déjà" + equipement + "!" );
+			case 3:
+				
+			default:
+				System.out.println("choix incorrect");
+				break;
+	}
+	
 	}
 public static void main(String[] args) {
 	Romain cesar = new Romain("cesar", 6);
